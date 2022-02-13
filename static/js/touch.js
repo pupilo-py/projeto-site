a1d1edf0c68c9955c596f0597de34583c0091dcd
pupilo-py <jv.13wap@gmail.com>
2022-02-12 21:08:53 -0300

    reponsividade

---
+++ static/js/touch.js
@@ -0,0 +1,20 @@
+var a = window.document.querySelector("a")
+var h3_a = window.document.querySelector("h3 > a")
+
+a.ontouchstart = () => {
+    a.style.color = "#3a0070"
+    a.style.textDecoration = "underline"
+}
+
+a.ontouchend = () => {
+    a.style.color = "#1a1aff"
+}
+
+h3_a.ontouchstart =  () => {
+    h3_a.style.color = "#1a1aff"
+    h3_a.style.textDecoration = "underline"
+}
+
+h3_a.ontouchend =  () => {
+    h3_a.style.color = "#3a0070"
+}
