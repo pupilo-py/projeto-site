8d6c1e7e564d1c9bbf3a2d761162dcd919305d20
pupilo-py <jv.13wap@gmail.com>
2022-02-12 22:23:20 -0300

    modifiquei o js

--- static/js/touch.js
+++ static/js/touch.js
@@ -2,23 +2,23 @@ var a = window.document.querySelectorAll("a")
 var h3_a = window.document.querySelector("h3 > a")
 
 a[0].ontouchstart = () => {
-    a.style.color = "#1a1aff"
-    a.style.textDecoration = "underline"
+    a[0].style.color = "#1a1aff"
+    a[0].style.textDecoration = "underline"
 }
 
 a[0].ontouchend = () => {
-    a.style.color = "#3a0070"
-    a.style.textDecoration = "none"
+    a[0].style.color = "#3a0070"
+    a[0].style.textDecoration = "none"
 }
 
 a[1].ontouchstart = () => {
-    a.style.color = "#1a1aff"
-    a.style.textDecoration = "underline"
+    a[1].style.color = "#1a1aff"
+    a[1].style.textDecoration = "underline"
 }
 
 a[1].ontouchend = () => {
-    a.style.color = "#3a0070"
-    a.style.textDecoration = "none"
+    a[1].style.color = "#3a0070"
+    a[1].style.textDecoration = "none"
 }
 
 h3_a.ontouchstart = () => {
